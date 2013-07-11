c3328f31936abbfc3c909a6f80e082fdb4e8ad49
Yannick Dawant <ydawant@gmail.com>
2013-07-11 16:32:32 -0500

    trying to fix github spacing issue

--- one_die_game.rb
+++ one_die_game.rb
@@ -1,59 +1,57 @@
 class Player
+  attr_reader :name
 
-	attr_reader :name
+  def initialize(name)
+    @name = name
+  end
 
-	def initialize(name)
-		@name = name
-	end
-
-	def pick_next_move(die, current_score)
-		puts "please pick between #{die.possible_moves}:"
-		choice = gets.chomp.to_i
-		if die.valid_choice(choice)
-			die.current_face = choice
-		else
-			puts "Pick a valid number!"
-			pick_next_move(die, current_score)
-		end
-		choice
-	end
+  def pick_next_move(die, current_score)
+    puts "please pick between #{die.possible_moves}:"
+    choice = gets.chomp.to_i
+    if die.valid_choice(choice)
+      die.current_face = choice
+    else
+      puts "Pick a valid number!"
+      pick_next_move(die, current_score)
+    end
+    choice
+  end
 
 end
 
 class Die
+  attr_accessor :current_face
 
-	attr_accessor :current_face
+  def initialize
+    @current_face = roll
+  end
 
-	def initialize
-		@current_face = roll
-	end
+  def roll
+    rand(6) + 1
+  end
 
-	def roll
-		rand(6) + 1
-	end
+  def possible_moves
+    (1..6).to_a.reject { |x| x == current_face || x == 7 - current_face } 
+  end
 
-	def possible_moves
-		(1..6).to_a.reject { |x| x == current_face || x == 7 - current_face } 
-	end
-
-	def valid_choice(choice)
-		possible_moves.any? { |x| x == choice }
-	end
+  def valid_choice(choice)
+    possible_moves.any? { |x| x == choice }
+  end
 
 end
 
 class Computer
 
-	def next_move(die, current_score)
-		points_left = 31 - current_score
-		puts "computer must pick between #{die.possible_moves}:"
-		sleep(0.5)
-		if can_win?(die, points_left)
-			computer_pick = pick_winning_number(die, points_left)
-		else
-			computer_pick = die.possible_moves.sample
-			die.current_face = computer_pick 
-		end
+  def next_move(die, current_score)
+    points_left = 31 - current_score
+    puts "computer must pick between #{die.possible_moves}:"
+    sleep(0.5)
+    if can_win?(die, points_left)
+      computer_pick = pick_winning_number(die, points_left)
+    else
+      computer_pick = die.possible_moves.sample
+      die.current_face = computer_pick 
+    end
    puts "the computer chose #{computer_pick}"
    computer_pick
  end
@@ -69,62 +67,61 @@ end
 end
 
 class Game
+  attr_accessor :current_score
+
+  def initialize
+    p "Welcome, challenger. Please enter your name:"
+    name = gets.chomp
+    @player = Player.new(name)
+    @die = Die.new
+    @computer = Computer.new
+    @max_score = 31
+    @current_score = @die.current_face
+    puts "#{@player.name} has started the game with a #{@current_score}"
+  end
+
+  def computer_play
+    @current_score += @computer.next_move(@die, @current_score)
+  end
+
+  def player_play
+    @current_score += @player.pick_next_move(@die, @current_score)
+  end
+
+  def play_game
+    until @current_score >= 31
+      computer_play
+      if computer_game_over?
+        break
+      end
+      puts "The current score is #{@current_score}" 
+      player_play
+      if player_game_over?
+        break
+      end
+    end
+  end
+
+  def computer_game_over?
+    if @current_score > 31
+      puts "#{@player.name} wins!"
+    elsif @current_score == 31
+      puts "Computer wins!"
+    end
+  end
+
+  def player_game_over?
+    if @current_score > 31
+      puts "Computer wins!"
+    elsif @current_score == 31
+      puts "#{@player.name} wins!"
+    end
+  end
   
-	attr_accessor :current_score
-
-	def initialize
-		p "Welcome, challenger. Please enter your name:"
-		name = gets.chomp
-		@player = Player.new(name)
-		@die = Die.new
-		@computer = Computer.new
-		@max_score = 31
-		@current_score = @die.current_face
-		puts "#{@player.name} has started the game with a #{@current_score}"
-	end
-
-	def computer_play
-		@current_score += @computer.next_move(@die, @current_score)
-	end
-
-	def player_play
-		@current_score += @player.pick_next_move(@die, @current_score)
-	end
-
-	def play_game
-		until @current_score >= 31
-			computer_play
-			if computer_game_over?
-				break
-			end
-			puts "The current score is #{@current_score}"	
-			player_play
-			if player_game_over?
-				break
-			end
-		end
-	end
-
-	def computer_game_over?
-		if @current_score > 31
-			puts "#{@player.name} wins!"
-		elsif @current_score == 31
-			puts "Computer wins!"
-		end
-	end
-
-	def player_game_over?
-		if @current_score > 31
-			puts "Computer wins!"
-		elsif @current_score == 31
-			puts "#{@player.name} wins!"
-		end
-	end
-	
 end
 
 if $PROGRAM_NAME == __FILE__
-	game = Game.new
-	game.play_game
+  game = Game.new
+  game.play_game
 end
 
--- test/one_die_game_test.rb
+++ test/one_die_game_test.rb
@@ -3,149 +3,152 @@ require 'rspec'
 
 describe 'Player' do
 
-	before do
-		@player = Player.new("Yannick") 
-		@die = Die.new
-		@die.current_face = 6
-	end
+  before do
+    @player = Player.new("Yannick") 
+    @die = Die.new
+    @die.current_face = 6
+  end
 
-	it "should require a name as input" do
-		expect {Player.new}.to raise_exception
-	end
+  it "should require a name as input" do
+    expect {Player.new}.to raise_exception
+  end
 
-	it "should not allow for a wrong number to be chosen" do
-		@player.stub!(:gets).and_return('7', '5')
-		printed = capture_out do
-			@player.pick_next_move(@die, 5)
-		end
-		expect(printed.chomp).to eq("please pick between [2, 3, 4, 5]:\nPick a valid number!\nplease pick between [2, 3, 4, 5]:")
-	end
+  it "should not allow for a wrong number to be chosen" do
+    @player.stub!(:gets).and_return('7', '5')
+    printed = capture_out do
+      @player.pick_next_move(@die, 5)
+    end
+    expect(printed.chomp).to eq("please pick between [2, 3, 4, 5]:\nPick a valid number!\nplease pick between [2, 3, 4, 5]:")
+  end
 
-	it "should return the chosen number if valid" do
-		@player.stub!(:gets).and_return('5')
-		@player.pick_next_move(@die, 5).should eq 5
-	end
+  it "should return the chosen number if valid" do
+    @player.stub!(:gets).and_return('5')
+    @player.pick_next_move(@die, 5).should eq 5
+  end
+
 end
 
 describe 'Die' do
 
-	before do
-		@die = Die.new
-		@die.current_face = 6
-	end
+  before do
+    @die = Die.new
+    @die.current_face = 6
+  end
 
-	it "should roll a number between 1-6" do
-		@die.roll.should be_between(1, 6)
-	end
+  it "should roll a number between 1-6" do
+    @die.roll.should be_between(1, 6)
+  end
 
-	it "should be able to tell what the possible choices are" do
-		@die.possible_moves.should eq [2,3,4,5]
-	end
+  it "should be able to tell what the possible choices are" do
+    @die.possible_moves.should eq [2,3,4,5]
+  end
 
-	it "should be able to determine if the choice by the user is valid" do
-		@die.valid_choice(5).should eq true
-	end
+  it "should be able to determine if the choice by the user is valid" do
+    @die.valid_choice(5).should eq true
+  end
 
-	it "should be able to determine if the choice by the user is invalid" do
-		@die.valid_choice(6).should eq false
-		@die.valid_choice(1).should eq false
-	end
+  it "should be able to determine if the choice by the user is invalid" do
+    @die.valid_choice(6).should eq false
+    @die.valid_choice(1).should eq false
+  end
 
-	it "should be able to determine if a number is outside the scope" do
-		@die.valid_choice(10).should eq false
-	end
+  it "should be able to determine if a number is outside the scope" do
+    @die.valid_choice(10).should eq false
+  end
+
 end
 
 
 describe 'Computer' do
 
-	before do
-		@computer = Computer.new
-		@die = Die.new
-		@die.current_face = 6
-	end
+  before do
+    @computer = Computer.new
+    @die = Die.new
+    @die.current_face = 6
+  end
 
-	it "should be able to determine it can not win on the next turn if too many points left" do
-		@computer.can_win?(@die, 12).should eq false
-	end
+  it "should be able to determine it can not win on the next turn if too many points left" do
+    @computer.can_win?(@die, 12).should eq false
+  end
 
-	it "should be able to determine it can not win on the next turn if the necessary number isn't available" do
-		@computer.can_win?(@die, 6).should eq false
-	end
+  it "should be able to determine it can not win on the next turn if the necessary number isn't available" do
+    @computer.can_win?(@die, 6).should eq false
+  end
 
-	it "should be able to determine it can win on the next turn" do
-		@computer.can_win?(@die, 5).should eq true
-	end
+  it "should be able to determine it can win on the next turn" do
+    @computer.can_win?(@die, 5).should eq true
+  end
 
-	it "should be able to pick the winning number" do
-		@computer.pick_winning_number(@die, 5).should eq 5
-	end
+  it "should be able to pick the winning number" do
+    @computer.pick_winning_number(@die, 5).should eq 5
+  end
 
-	it "should return a viable number if the winning number is not available" do
-		@computer.next_move(@die, 20).should be_between(2,5)
-	end
+  it "should return a viable number if the winning number is not available" do
+    @computer.next_move(@die, 20).should be_between(2,5)
+  end
 
-	it "should return the winning number if the winning number is not available" do
-		@computer.next_move(@die, 26).should eq 5
-	end
+  it "should return the winning number if the winning number is not available" do
+    @computer.next_move(@die, 26).should eq 5
+  end
+  
 end
 
 describe 'Game' do
 
-	before do
-		@game = Game.new
-	end
+  before do
+    @game = Game.new
+  end
 
-	it "should be able to determine if the computer DID NOT ended the game" do
-		@game.computer_game_over?.should eq nil
-	end
+  it "should be able to determine if the computer DID NOT ended the game" do
+    @game.computer_game_over?.should eq nil
+  end
 
-	it "should be able to determine if the computer ended and WON the game" do
-		@game.current_score = 31
-		printed = capture_out do
-			@game.computer_game_over?
-		end
-		expect(printed.chomp).to eq("Computer wins!")
-	end
+  it "should be able to determine if the computer ended and WON the game" do
+    @game.current_score = 31
+    printed = capture_out do
+      @game.computer_game_over?
+    end
+    expect(printed.chomp).to eq("Computer wins!")
+  end
 
-	it "should be able to determine if the computer ended and LOST the game" do
-		@game.current_score = 32
-		printed = capture_out do
-			@game.computer_game_over?
-		end
-		expect(printed.chomp).to eq(" wins!")
-	end
+  it "should be able to determine if the computer ended and LOST the game" do
+    @game.current_score = 32
+    printed = capture_out do
+      @game.computer_game_over?
+    end
+    expect(printed.chomp).to eq(" wins!")
+  end
 
-	it "should be able to determine if the player DID NOT end the game" do
-		@game.player_game_over?.should eq nil
-	end
+  it "should be able to determine if the player DID NOT end the game" do
+    @game.player_game_over?.should eq nil
+  end
 
-	it "should be able to determine if the player ended and WON the game" do
-		@game.current_score = 31
-		printed = capture_out do
-			@game.player_game_over?
-		end
-		expect(printed.chomp).to eq(" wins!")
-	end
+  it "should be able to determine if the player ended and WON the game" do
+    @game.current_score = 31
+    printed = capture_out do
+      @game.player_game_over?
+    end
+    expect(printed.chomp).to eq(" wins!")
+  end
 
-	it "should be able to determine if the player ended and LOST the game" do
-		@game.current_score = 32
-		printed = capture_out do
-			@game.player_game_over?
-		end
-		expect(printed.chomp).to eq("Computer wins!")
-	end
+  it "should be able to determine if the player ended and LOST the game" do
+    @game.current_score = 32
+    printed = capture_out do
+      @game.player_game_over?
+    end
+    expect(printed.chomp).to eq("Computer wins!")
+  end
 
 end
 
 def capture_out(&block)
-	old_stdout = $stdout
-	fake_stdout = StringIO.new
-	$stdout = fake_stdout
-	block.call
-	fake_stdout.string
+  old_stdout = $stdout
+  fake_stdout = StringIO.new
+  $stdout = fake_stdout
+  block.call
+  fake_stdout.string
 ensure
-	$stdout = old_stdout
+  $stdout = old_stdout
 end
 
 
